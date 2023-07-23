34ea6317c8e972eeef5bf2db4e265a35adba9c30
Saurabh Mishra <47188118+smishraGitHub@users.noreply.github.com>
2023-07-23 17:01:20 +0530

    Commit 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatRadioModule} from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,9 @@ import {MatRadioModule} from '@angular/material/radio';
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatRadioModule
+    MatRadioModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
-export class DialogComponent {
+export class DialogComponent implements OnInit {
 
-  freshnessList =["Level1","Level2","Level3"]
-  constructor(){}
+  freshnessList =["Level1","Level2","Level3"];
+  productForm !: FormGroup;
+
+
+  constructor(private formBuilder:FormBuilder){}
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+       productName:['',Validators.required],
+       category:['',Validators.required],
+       date:['',Validators.required],
+       freshness:['',Validators.required],
+       price:['',Validators.required],
+       comments:['',Validators.required],
+    });
+  }
+
+  addProduct(){
+    console.log(this.productForm.value);
+  }
 
 }
